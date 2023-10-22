b737ea86411920c6c2b7470b45b42ba4d3eb1dd4
Spring-0 <springsquash93@gmail.com>
2023-10-22 02:51:30 -0400

    Feature: Commands & Listeners Registration.
    - Register SumoListener
    - Register SumoCommands
    - Add Command to plugin.yml

--- src/main/java/dev/spring93/sumoevent/SumoEvent.java
+++ src/main/java/dev/spring93/sumoevent/SumoEvent.java
@@ -1,17 +1,38 @@
 package dev.spring93.sumoevent;
 
+import dev.spring93.sumoevent.commands.SumoCommands;
+import dev.spring93.sumoevent.listeners.SumoListener;
+import dev.spring93.sumoevent.services.GameService;
+import dev.spring93.sumoevent.utils.ConfigManager;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SumoEvent extends JavaPlugin {
 
+    private ConfigManager configManager;
+    private SumoListener sumoListener;
+    private GameService gameService;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        configManager = ConfigManager.getInstance();
+        registerListeners();
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    private void registerCommands() {
+        this.getCommand("sumo").setExecutor(new SumoCommands());
+    }
+
+    private void registerListeners() {
+        sumoListener = new SumoListener();
+        this.getServer().getPluginManager().registerEvents(sumoListener, this);
+    }
+
+
 }
