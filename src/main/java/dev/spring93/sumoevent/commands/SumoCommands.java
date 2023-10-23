3b0acb7e74db7d934df60762f691fd886b1b069f
Spring-0 <springsquash93@gmail.com>
2023-10-23 17:20:03 -0400

    Feature: Implement command permissions.

--- src/main/java/dev/spring93/sumoevent/commands/SumoCommands.java
+++ src/main/java/dev/spring93/sumoevent/commands/SumoCommands.java
@@ -1,17 +1,20 @@
 package dev.spring93.sumoevent.commands;
 
-import dev.spring93.sumoevent.listeners.SumoListener;
 import dev.spring93.sumoevent.services.GameService;
+import dev.spring93.sumoevent.utils.ConfigManager;
+import dev.spring93.sumoevent.utils.MessageManager;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class SumoCommands extends BaseCommand {
 
-    GameService gameService;
+    private GameService gameService;
+    private ConfigManager config;
 
     public SumoCommands() {
         super("sumo");
         gameService = new GameService();
+        config = ConfigManager.getInstance();
     }
 
     @Override
@@ -20,10 +23,14 @@ public class SumoCommands extends BaseCommand {
 
         switch (arg1) {
             case "start":
-                gameService.startGame(sender);
+                if(sender.hasPermission("sumoevent.command.start"))
+                    gameService.startGame(sender);
+                else MessageManager.sendMessage(sender, config.getNoPermissionMessage());
                 break;
             case "stop":
-                gameService.stopGame(sender);
+                if(sender.hasPermission("sumoevent.command.start"))
+                    gameService.stopGame(sender);
+                else MessageManager.sendMessage(sender, config.getNoPermissionMessage());
                 break;
             case "join":
                 gameService.addPlayer((Player) sender);
@@ -32,10 +39,22 @@ public class SumoCommands extends BaseCommand {
                 gameService.removePlayer((Player) sender);
                 break;
             case "startmatch":
-                gameService.startMatch();
+                if(sender.hasPermission("sumoevent.command.startmatch"))
+                    gameService.startMatch();
+                else MessageManager.sendMessage(sender, config.getNoPermissionMessage());
+                break;
+            case "help":
+                if(sender.hasPermission("sumoevent.command.help"))
+                    sender.sendMessage(MessageManager.getHelpMenu());
+                else MessageManager.sendMessage(sender, config.getNoPermissionMessage());
+                break;
+            case "reload":
+                if(sender.hasPermission("sumoevent.command.reload"))
+                    config.reloadConfig(sender);
+                else MessageManager.sendMessage(sender, config.getNoPermissionMessage());
                 break;
             default:
-                sender.sendMessage("Invalid argument.");
+                MessageManager.sendMessage(sender, config.getInvalidArgMessage());
         }
 
 
