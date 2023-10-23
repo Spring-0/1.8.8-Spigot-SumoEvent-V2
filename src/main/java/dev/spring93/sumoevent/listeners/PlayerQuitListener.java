b662b43198a835ca0bb87757ad95e1dc44cc648a
Spring-0 <springsquash93@gmail.com>
2023-10-22 20:37:23 -0400

    Feature: Implement player logout during event handler.
    - Forfeits the player on logout and removes them from the queue.

--- src/main/java/dev/spring93/sumoevent/listeners/PlayerQuitListener.java
+++ src/main/java/dev/spring93/sumoevent/listeners/PlayerQuitListener.java
@@ -1,4 +1,24 @@
 package dev.spring93.sumoevent.listeners;
 
-public class PlayerQuitListener {
+import dev.spring93.sumoevent.services.GameService;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener implements Listener {
+
+    GameService gameService = new GameService();
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        gameService.removePlayerFromQueue(player);
+
+        if(player.equals(gameService.getPlayer1()))
+            gameService.endMatch(gameService.getPlayer2(), player);
+        else if(player.equals(gameService.getPlayer2()))
+            gameService.endMatch(gameService.getPlayer1(), player);
+    }
+
 }
