b21508117920f710692954eaf0ba577a3d99994e
Spring-0 <springsquash93@gmail.com>
2023-10-24 02:06:21 -0400

    BugFix: Minor `SumoEventFlareUseListener` changes.
    - Prevent placement of item on summoned.
    - Prevent use of item while sumo event is active.

--- src/main/java/dev/spring93/sumoevent/listeners/SumoEventFlareUseListener.java
+++ src/main/java/dev/spring93/sumoevent/listeners/SumoEventFlareUseListener.java
@@ -1,7 +1,9 @@
 package dev.spring93.sumoevent.listeners;
 
 import dev.spring93.sumoevent.items.SumoEventFlare;
+import dev.spring93.sumoevent.services.GameService;
 import dev.spring93.sumoevent.utils.ConfigManager;
+import dev.spring93.sumoevent.utils.MessageManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,9 +16,11 @@ import java.util.List;
 public class SumoEventFlareUseListener implements Listener {
 
     private ConfigManager config;
+    private GameService gameService;
 
     public SumoEventFlareUseListener() {
         config = ConfigManager.getInstance();
+        gameService = new GameService();
     }
 
     @EventHandler
@@ -26,13 +30,23 @@ public class SumoEventFlareUseListener implements Listener {
         ItemStack sumoFlare = new SumoEventFlare().getSumoFlare();
 
         if(item != null && item.isSimilar(sumoFlare) && event.getAction().name().contains("RIGHT")) {
+
+            if(gameService.isGameActive()) {
+                event.setCancelled(true);
+                MessageManager.sendMessage(player, config.getEventAlreadyRunningMessage());
+                return;
+            }
+
             ItemStack singleFlare = sumoFlare.clone();
             singleFlare.setAmount(1);
             player.getInventory().removeItem(singleFlare);
             List<String> commands = config.getSumoFlareCommands();
+
+            gameService.startGame(player);
             for (String command : commands) {
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", player.getName()));
             }
+            event.setCancelled(true);
         }
 
     }
